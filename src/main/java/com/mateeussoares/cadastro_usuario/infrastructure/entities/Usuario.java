2bb9eed7deb058bddc10fcf8e0445ce61ed34fa7
mateeussoares <mateussoaresss13@gmail.com>
2026-02-08 17:16:59 -0300

    first commit

---
+++ src/main/java/com/mateeussoares/cadastro_usuario/infrastructure/entities/Usuario.java
@@ -0,0 +1,25 @@
+package com.mateeussoares.cadastro_usuario.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "usuario")
+@Entity
+
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "email", unique =  true)
+    private String email;
+
+    @Column(name = "name")
+    private String nome;
+}
---
+++ src/main/java/com/mateeussoares/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.mateeussoares.cadastro_usuario.infrastructure.repository;
+
+import com.mateeussoares.cadastro_usuario.infrastructure.entities.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
